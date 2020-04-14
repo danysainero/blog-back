0f9e804cde7d7d84b43ac41c7892b7923d6a9b81
Dany <daniel.sainero@kairosds.com>
2020-04-14 11:18:01 +0200

    init project

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+ module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+  };
+
+  
