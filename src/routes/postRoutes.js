7b6adbe4b9695e83959396aeafdbb8aac0374dc7
Dany <daniel.sainero@kairosds.com>
2020-04-23 18:22:09 +0200

    jwt added to posts endpoints POST, PUT & DELETE

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -26,14 +26,10 @@ postRouter.use(passport.initialize());
 
 
 /* postRouter.get('/posts',PostController.getAllPosts); */
-postRouter.get(
-  '/posts',
-  passport.authenticate('jwt', { session: false }),
-  PostController.getAllPosts
-);
+postRouter.get('/posts',PostController.getAllPosts);
 postRouter.get('/posts/:id', PostController.getPostById);
-postRouter.post('/posts', PostController.createPost);
-postRouter.put('/posts/:id', PostController.modifyPost);
-postRouter.delete('/posts/:id', PostController.deletePost);
+postRouter.post('/posts', passport.authenticate('jwt', { session: false }), PostController.createPost);
+postRouter.put('/posts/:id', passport.authenticate('jwt', { session: false }), PostController.modifyPost);
+postRouter.delete('/posts/:id', passport.authenticate('jwt', { session: false }), PostController.deletePost);
 
 module.exports = postRouter;
